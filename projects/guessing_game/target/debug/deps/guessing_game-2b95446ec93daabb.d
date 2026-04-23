3d403a1d7a468f53af49440a4315e998ee71a9a5
Brandon Turner <bturner218@outlook.com>
2026-04-23 19:19:29 -0400

    Started chapter 2

---
+++ projects/guessing_game/target/debug/deps/guessing_game-2b95446ec93daabb.d
@@ -0,0 +1,5 @@
+/home/brandont/developer/rust-book-code/projects/guessing_game/target/debug/deps/guessing_game-2b95446ec93daabb.d: src/main.rs
+
+/home/brandont/developer/rust-book-code/projects/guessing_game/target/debug/deps/guessing_game-2b95446ec93daabb: src/main.rs
+
+src/main.rs:
---
+++ projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/brandont/developer/rust-book-code/projects/guessing_game/target/debug/guessing_game: /home/brandont/developer/rust-book-code/projects/guessing_game/src/main.rs
